b5b065658ffa1b71a6529153cd6eee1d4716542e
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-28 19:24:09 +0545

    added reset button functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let typecontent=document.querySelector(".type-content p");
 let input = document.getElementById('typeValue');
 let letterindex = (mistakes = isTyping = 0);
+let resetBtn = document.querySelector('.bottom-part button');
 
 let time;
 let tleft= document.querySelector('.time-left');
@@ -16,10 +17,13 @@ let timeleft = maxTime;
 
 const loadpara = () => {
     let randompara = Math.floor(Math.random() * article.length); 
+    typecontent.innerHTML = "";
     article[randompara].split('').forEach(element => {
         let realdata= `<span>${element}</span>`;
         typecontent.innerHTML += realdata;
     })
+
+    typecontent.querySelectorAll('span')[0].classList.add('active');
     
     document.addEventListener('click', ()=> {
         input.focus();
@@ -59,6 +63,8 @@ input.addEventListener('input',(e) => {
             })
 
             char[letterindex].classList.add('active');
+            cpm.innerText = `CPM :${letterindex - mistakes}`;
+
             }
          } else{
 
@@ -71,6 +77,20 @@ const timesetup = () => {
     if(timeleft > 0){
 
         timeleft--;
-        tleft.innerText = `Time-left : ${timeleft}`;
+        tleft.innerText = `Time-left : ${timeleft}S`;
+        let wpmval= Math.round((letterindex - mistakes) / 5 / (maxTime - timeleft) * 60);
+        wpm.innerText=`WPM : ${wpmval}`;
     }
 }
+
+resetBtn.addEventListener('click', () => {
+    loadpara();
+    clearInterval(time);
+    wpm.innerText =`WPM : `;
+    error.innerText = `Mistakes : `;
+    cpm.innerText =`CPM :`;
+    timeleft= maxTime;
+    tleft.innerText =`Time-left:${maxTime}s`;
+    input.value="";
+    letterindex = mistakes = isTyping = 0;
+})
